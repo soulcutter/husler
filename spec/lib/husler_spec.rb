ad996117cdcbe6feaac6e1a1bf025f62221b998b
Bradley Schaefer <bradley.schaefer@gmail.com>
2012-10-14 00:47:48 -0500

    Changed spec to allow minor deviation because of differences in floating point math on jruby

--- spec/lib/husler_spec.rb
+++ spec/lib/husler_spec.rb
@@ -1,18 +1,27 @@
 require 'spec_helper'
 
 describe Husler do
+  let(:deviation) { 1.0e-13 }
+
   context "#rgb_to_husl" do
     it "matches the expected output from the canonical coffeescript implementation" do
       # jQuery.husl.fromRGB(0.75, 0.5, 0.25);
-      Husler.rgb_to_husl(0.75, 0.5, 0.25).should == [41.92580295066964, 78.60025347503503, 58.774191389329374]
+      husl = Husler.rgb_to_husl(0.75, 0.5, 0.25)
+
+      husl[0].should be_within(deviation).of(41.92580295066964)
+      husl[1].should be_within(deviation).of(78.60025347503503)
+      husl[2].should be_within(deviation).of(58.774191389329374)
     end
   end
 
   context "#husl_to_rgb" do
     it "matches the expected output from the canonical coffeescript implementation" do
       # jQuery.husl.toRGB(41.92580295066964, 78.60025347503503, 58.774191389329374)
-      Husler.husl_to_rgb(41.92580295066964, 78.60025347503503, 58.774191389329374).should ==
-          [0.7499979858220194, 0.5000211204153698, 0.25000732006691057]
+      rgb = Husler.husl_to_rgb(41.92580295066964, 78.60025347503503, 58.774191389329374)
+
+      rgb[0].should be_within(deviation).of(0.7499979858220194)
+      rgb[1].should be_within(deviation).of(0.5000211204153698)
+      rgb[2].should be_within(deviation).of(0.25000732006691057)
     end
   end
 end
